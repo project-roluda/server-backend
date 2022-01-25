e7e3ce748167b5565d1b891169dbe7bfe0f08f16
The Floating String <laurencel2001@gmail.com>
2022-01-25 00:13:12 -0500

    Prep for deployment to Azure web app

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, request
+import joblib
+import librosa
+import numpy as np
 
 app = Flask(__name__)
 
+result_proba_dict = {}
+
 @app.route("/", methods=["GET"])
 def home():
-    return {"data": True}
+    return result_proba_dict
+
+@app.route("/process_audio", methods=["POST"])
+def process_audio():
+    global result_proba_dict
+    audio_file = request.files["file"]
+    audio_file.save("static/sample_audio.wav")
+
+    ml_model = joblib.load("static/knn.sav")
+
+    sound, sample_rate = librosa.load("static/sample_audio.wav")
+    initial_coeff = librosa.feature.mfcc(y=sound, sr=sample_rate, n_mfcc=100)
+    avg_initial_coeff = np.mean(initial_coeff, axis=1)
+    
+    res = ml_model.predict_proba([avg_initial_coeff])
+
+    labels = ml_model.classes_
+
+    result_proba_dict = {}
+
+    print(labels)
+
+    for i in range(len(labels)):
+        result_proba_dict[labels[i]] = res[0][i]
+
+    return result_proba_dict
+
+
+@app.route("/show_status", methods=["GET"])
+def show_status():
+    return "200"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
